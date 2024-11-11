1458d3b69ba9e676312cdd2e0fa9fecfea9ebe1c
Guilherme Braga <89932943+guilhermevnbraga@users.noreply.github.com>
2024-11-11 18:02:17 -0300

    feat: Add text shadow to header

--- app/ui/header.tsx
+++ app/ui/header.tsx
@@ -7,19 +7,19 @@ export default function Header() {
         <header className="flex w-full justify-between px-16 py-2 bg-[#1C1C1C]">
             <Image src={logo} alt="Logo" width={80} height={80} />
             <nav className="flex gap-24 text-3xl">
-                <button className="bg-gradient-to-r from-[#8F612D] via-[#EADF86] to-[#8F612D] text-transparent bg-clip-text hover:scale-105 active:scale-95">
+                <button className="bg-gradient-to-r from-[#8F612D] via-[#EADF86] to-[#8F612D] text-transparent bg-clip-text hover:scale-105 active:scale-95 text-shadow">
                     Home
                 </button>
-                <button className="bg-gradient-to-r from-[#8F612D] via-[#EADF86] to-[#8F612D] text-transparent bg-clip-text hover:scale-105 active:scale-95">
+                <button className="bg-gradient-to-r from-[#8F612D] via-[#EADF86] to-[#8F612D] text-transparent bg-clip-text hover:scale-105 active:scale-95 text-shadow">
                     About
                 </button>
-                <button className="bg-gradient-to-r from-[#8F612D] via-[#EADF86] to-[#8F612D] text-transparent bg-clip-text hover:scale-105 active:scale-95">
+                <button className="bg-gradient-to-r from-[#8F612D] via-[#EADF86] to-[#8F612D] text-transparent bg-clip-text hover:scale-105 active:scale-95 text-shadow">
                     Projects
                 </button>
-                <button className="bg-gradient-to-r from-[#8F612D] via-[#EADF86] to-[#8F612D] text-transparent bg-clip-text hover:scale-105 active:scale-95">
+                <button className="bg-gradient-to-r from-[#8F612D] via-[#EADF86] to-[#8F612D] text-transparent bg-clip-text hover:scale-105 active:scale-95 text-shadow">
                     Contact
                 </button>
-                <button className="-ml-8">
+                <button className="-ml-6">
                     <Image src={brasil} alt="Brasil" width={60} height={60} />
                 </button>
             </nav>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import type { PluginAPI } from "tailwindcss/types/config";
 
 const config: Config = {
   content: [
@@ -12,8 +13,35 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      textShadow: {
+        'default': '2px 2px 4px rgba(0, 0, 0, 0.5)',
+        'md': '3px 3px 6px rgba(0, 0, 0, 0.5)',
+        'lg': '4px 4px 8px rgba(0, 0, 0, 0.5)',
+      }
     },
   },
-  plugins: [],
+  variants: {
+    extend: {
+      textShadow: ['responsive', 'hover', 'focus'],
+    },
+  },
+  plugins: [
+    function (api: PluginAPI) {
+      const newUtilities = {
+        '.text-shadow': {
+          textShadow: '0px 2px 12px rgba(143, 97, 45, 1)',
+        },
+        '.text-shadow-md': {
+          textShadow: '3px 3px 6px rgba(143, 97, 45, 1)',
+        },
+        '.text-shadow-lg': {
+          textShadow: '4px 4px 8px rgba(143, 97, 45, 1)',
+        },
+      }
+
+      api.addUtilities(newUtilities, { respectPrefix: false, respectImportant: false })
+    }
+  ],
 };
+
 export default config;
